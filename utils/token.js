b8c32383f721ef8b67082284eea1ad1fadfe79f4
nyko1 <yaokoua1@gmail.com>
2024-07-11 04:03:57 +0000

    retour a 1h temps du token

--- utils/token.js
+++ utils/token.js
@@ -7,7 +7,7 @@ dotenv.config();
 // Fonction pour générer un token JWT
 const generateToken = (user) => {
   const payload = { id: user.IDUSER, username: user.USERNAME, role: user.ROLE };
-  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1m' });
+  return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
 };
 
 
